57527533063150cea99925355983205ca8ec8819
Tomasz Adamiec <adamiec.tomas@gmail.com>
2013-05-20 21:13:16 +0200

    poprawienie  obrazu rozwiazan oraz raportu

--- rysunek.m
+++ rysunek.m
@@ -1,18 +1,24 @@
 res = zeros(121, 2);
 
+x = linprog(fA, A, b, Aeq, beq);
+
 n = 1;
 global cAsp;
 global rAsp;
 global e;
 for rAsp = 0:40:400
     for cAsp = 0:40:400
+        cAsp
+        rAsp
         e = 0.1;
         y = fminimax(@f, x, A, b, Aeq, beq);
+        temp = rAsp;
         rAsp = 0;
         cAsp = 0;
         e = 0;
         res(n, :) = f(y);
         n = n + 1;
+        rAsp = temp;
     end
 end
 
